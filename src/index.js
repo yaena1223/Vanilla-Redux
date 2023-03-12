0ca43e920c706729f2b708358c9c0b411b56b152
yaena1223 <yaena1223@dgu.ac.kr>
2023-03-12 21:57:24 +0900

    feat: actions

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import { legacy_createStore } from 'redux';
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
-let count = 0;
-number.innerText = count;
-const updateText = () => {
-  number.innerText = count;
-};
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  }
+  return count;
 };
+//reducer는 data를 modify하는 function임
+//위에서는 reducer의 함수명을 countModifier로 했고
+//countModifier가 return하는 값은 data가 된다
+const countStore = legacy_createStore(countModifier);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+console.log(countStore.getState());
