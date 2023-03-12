a5ce794fc600cd998a093cb508ed57d85627385e
yaena1223 <yaena1223@dgu.ac.kr>
2023-03-12 22:08:42 +0900

    feat: subscriptions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { legacy_createStore } from 'redux';
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
-
+number.innerText = '0';
 const countModifier = (count = 0, action) => {
+  console.log(action);
   if (action.type === 'ADD') {
     return count + 1;
   } else if (action.type === 'MINUS') {
@@ -15,11 +16,18 @@ const countModifier = (count = 0, action) => {
 //위에서는 reducer의 함수명을 countModifier로 했고
 //countModifier가 return하는 값은 data가 된다
 const countStore = legacy_createStore(countModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'MINUS' });
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+//subscribe는 state의 변화를 알 수 있게 해줌
+const handleAdd = () => {
+  countStore.dispatch({ type: 'ADD' });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: 'MINUS' });
+};
+add.addEventListener('click', () => handleAdd());
+minus.addEventListener('click', () => handleMinus());
